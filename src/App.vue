1efb81c12d344c214ab83c4b8d7e3c8cc59e78fd
Steve Kain <stevekain@gmail.com>
2018-01-06 21:57:00 -0600

    Better factoring?

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,10 @@
 <template>
-    <div id="app">
-        <!-- <img src="./assets/logo.png"> -->
-        <div id="app" class='pure-g'>
-            <div id='main' class='pure-u-4-5'>
-                <intro v-bind:game="game"></intro>
-            </div>
-            <div id='sidebar' class='pure-u-1-5'>
-                {{ message }}
-            </div>
+    <div id="app" class='pure-g'>
+        <div id='main' class='pure-u-4-5'>
+            <component v-bind:is='currentMainComponent' v-bind:game='game'></component>
+        </div>
+        <div id='sidebar' class='pure-u-1-5'>
+            <component v-bind:is='currentSidebarComponent' v-bind:game='game'></component>
         </div>
     </div>
 </template>
@@ -19,16 +16,36 @@ import CrabbinGame from './game/game_main';
 const game = new CrabbinGame();
 
 export default {
-  name: 'app',
-  components: {
-      'intro': Intro
-  },
-  data () {
-    return {
-      message: 'Welcome to Your Vue.js App',
-      game: game
+    name: 'app',
+    components: {
+        'intro': Intro
+    },
+    data() {
+        return {
+            message: 'Welcome to Your Vue.js App',
+            game: game
+        }
+    },
+    computed: {
+        currentMainComponent() {
+            if (this.game.showIntro) {
+                return 'intro';
+            } else if (this.game.started) {
+                return '';
+            } else if (this.game.ended) {
+                return '';
+            }
+        },
+        currentSidebarComponent() {
+            if (this.game.showIntro) {
+                return '';
+            } else if (this.game.started) {
+                return '';
+            } else if (this.game.ended) {
+                return '';
+            }
+        }
     }
-  }
 }
 </script>
 
--- src/game/game_main.js
+++ src/game/game_main.js
@@ -1,8 +1,13 @@
 export default class CrabbinGame {
     constructor() {
+        this.showIntro = true;
+        this.started = false;
+        this.ended = false;
     }
 
     start() {
-        alert('here');
+        this.showIntro = false;
+        this.started = true;
+        this.ended = false;
     }
 }
