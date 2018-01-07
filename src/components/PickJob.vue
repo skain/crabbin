6f8d833fff2849a474c74a785d9177915953c731
Steve Kain <stevekain@gmail.com>
2018-01-07 12:29:40 -0600

    Basic job selection complete.

--- src/components/PickJob.vue
+++ src/components/PickJob.vue
@@ -2,7 +2,7 @@
     <div>
         <h1>Pick a job</h1>
         <p v-if='game.is_greenhorn'>
-            OK, greenhorn. You're lucky enough to have been offered {{ available_jobs.length }} jobs! Things must really be tough up in Alaska.
+            OK, greenhorn. You're lucky enough to have been offered {{ availableJobs.length }} jobs! Things must really be tough up in Alaska.
             Remember, the bigger the boat the more likely it is you'll catch some crab but the lower your percentage payout will be. And this year, since
             you're a greenhorn, you'll only get 1% of the catch anyway.
         </p>
@@ -10,12 +10,20 @@
             Choose a job from the ones below:
         </p>
         <ul>
-            <li v-for='job in available_jobs'>
-                <p>Boat Name: {{ job.boatName }}</p>
-                <p>Boat Size: {{ job.boatSize }}</p>
-                <p>Crew Size: {{ job.crewSize }}</p>
+            <li v-for='job in availableJobs' v-on:click='selectJob(job)' v-bind:class='{ selected: job === selectedJob }'>
+                <div>Boat Name: {{ job.boatName }}</div>
+                <div>Boat Size: {{ job.boatSize }}</div>
+                <div>Crew Size: {{ job.crewSize }}</div>
             </li>
         </ul>
+        <div v-if='selectedJob'>
+            <p>
+                You've selected the {{ selectedJob.boatName }}. It's a fine ship.
+            </p>
+            <p>
+                <button v-on:click='setSelectedJob'>Sign up!</button>
+            </p>
+        </div>
     </div>
 </template>
 
@@ -24,12 +32,19 @@ export default {
     name: 'pick-job',
     data() {
         return {
-            available_jobs: this.generate_jobs()
+            availableJobs: this.generate_jobs(),
+            selectedJob: null
         }
     },
     methods: {
         generate_jobs() {
             return this.game.generate_jobs();
+        },
+        selectJob(job) {
+            this.selectedJob = job;
+        },
+        setSelectedJob() {
+            this.game.current_job = this.selectedJob;
         }
     },
     computed: {
@@ -39,5 +54,10 @@ export default {
 </script>
 
 <style scoped>
-
+    li {
+        padding: 10px;
+    }
+    li.selected {
+        background-color: #ffb5b5;
+    }
 </style>
